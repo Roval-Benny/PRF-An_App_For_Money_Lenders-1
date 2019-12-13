763de3045817554e3409680192e7ef4533ec0df7
Roval Benny <48195902+Roval-Benny@users.noreply.github.com>
2019-12-13 15:14:29 +0530

    updated

--- app/src/main/java/com/prf/prf/Global2.kt
+++ app/src/main/java/com/prf/prf/Global2.kt
@@ -5,7 +5,7 @@ class Global2 {
 
     companion object Chosen{
         var globalList:ArrayList<Users>? = null
-
+        
         fun returnGlobals(): ArrayList<Users> {
 
             return globalList!!
